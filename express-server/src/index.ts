91c3b19096340907d5dd1aa128b02710dc9a9dfe
Devender Singh <devendersingh2k@gmail.com>
2024-12-05 21:16:58 +0530

    express server completed

---
+++ express-server/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { createClient } from 'redis';
+
+
+const client = createClient();
+const app = express();
+app.use(express.json());
+
+app.post('/submit', (req, res)=>{
+    const { problemID, userId, code, language } = req.body();
+
+    // ideally you should push in the DB here like - prisma.submissions.create({})
+
+    client.lPush("submissions", JSON.stringify({problemID, userId, code, language}));
+    res.json({
+        msg : "Submission Recieved!"
+    })
+})
+
+async function startServer(){
+    try{
+        await client.connect();
+        console.log("connected to redis")
+
+        app.listen(3000, ()=>{
+            console.log('server is running on port: 3000')
+        })
+    }catch(err){
+        console.log("error while connecting to redis")
+    }
+}
+
+startServer();
